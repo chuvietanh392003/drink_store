2d5844dc9588ad239e75826e440b4c72b0e9b43a
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-07-07 16:04:05 +0700

    new

---
+++ main.js
@@ -0,0 +1,6 @@
+// Chức năng thêm sản phẩm vào giỏ hàng
+function addToCart(productId) {
+    // Xử lý logic thêm sản phẩm vào giỏ hàng
+    alert('Thêm sản phẩm ' + productId + ' vào giỏ hàng thành công!');
+  }
+  
